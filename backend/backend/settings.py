8cbcee5ca72cfcc3ffa0c2718dea4bae0d9fd5eb
Meraj Ahmed <57950583+m879@users.noreply.github.com>
2021-09-19 21:27:24 +0530

    update settings.py
    email credentials removed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'merajahmed2112@gmail.com'
-EMAIL_HOST_PASSWORD = 'cumcgppublceiube'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
